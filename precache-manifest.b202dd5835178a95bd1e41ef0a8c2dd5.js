3bcf3b84103168e8807ea1a9794d6f8cbdc67288
joestar-dev <joestar-dev@users.noreply.github.com>
2021-07-17 17:15:45 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.b202dd5835178a95bd1e41ef0a8c2dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d0d190c52e05ba1faf805c9ae5528a",
+    "url": "https://joestar-dev.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "31eae0e8cbb7ab8a45bd",
+    "url": "https://joestar-dev.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "f59fea55f570f2022a9c",
+    "url": "https://joestar-dev.github.io/task-management-frontend/static/js/2.d3c9b229.chunk.js"
+  },
+  {
+    "revision": "31eae0e8cbb7ab8a45bd",
+    "url": "https://joestar-dev.github.io/task-management-frontend/static/js/main.8a2a9655.chunk.js"
+  },
+  {
+    "revision": "e82b5a4395b3ea82c02d",
+    "url": "https://joestar-dev.github.io/task-management-frontend/static/js/runtime~main.9ab47844.js"
+  }
+]);
